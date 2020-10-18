05915df308122da7984ce352be9b72fb26113520
schumyd <schumy.daraqutni21@gmail.com>
2020-10-18 19:52:15 +0700

    Pilah Sampah

---
+++ Assets/Scripts/Gameover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameover : MonoBehaviour {
+
+	float timer = 0;
+	// Use this for initialization
+	void Start () {
+		timer += Time.deltaTime;
+		if (timer > 2)
+			{
+			  Data.score = 0;
+			  SceneManager.LoadScene("Gameplay");
+		    }
+		
+	}
+}
